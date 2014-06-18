28ecac78143ddeff417b097c14393bf71332a338
Josh Cross <josh.ftscreations@gmail.com>
2014-06-18 17:17:52 -0500

    v 0.15

--- gen/com/example/sensorpack/R.java
+++ gen/com/example/sensorpack/R.java
@@ -1105,7 +1105,8 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int activity_main=0x7f030017;
         public static final int fragment_main=0x7f030018;
-        public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int list_layout=0x7f030019;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
@@ -1157,6 +1158,7 @@ containing a value of this type.
         public static final int app_name=0x7f0a000d;
         public static final int bluetoothstatus=0x7f0a000e;
         public static final int hello_world=0x7f0a0010;
+        public static final int list_is_empty=0x7f0a0015;
         public static final int off=0x7f0a0012;
         public static final int on=0x7f0a0011;
     }
